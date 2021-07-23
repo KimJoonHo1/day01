b8638f23c0ed300d83b8c05043a2ab53cba3a6fd
KJH <wnsgh9978@naver.com>
2021-07-23 11:32:06 +0900

    Ex02 생성

---
+++ src/day01/Ex02.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 \n : 엔터와 비슷하다
+		 \t : 탭크기만큼 오른쪽으로 이동(윈도우에서는 대략 8칸
+		 \" : "(큰따옴표)표시
+		 \\ \(역슬러시)표시
+		 */
+	}
+
+}
